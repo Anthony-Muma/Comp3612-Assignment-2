173044e24313a6705ebc1fc214bae94edc1ef485
Jack Le <jack.le.031231@hotmail.com>
2025-11-24 16:42:42 -0700

    I HATE STYLING

--- main.js
+++ main.js
@@ -224,7 +224,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const productData = localStorage.getItem("productData")
 
     if (!productData) {
-        fetch("./data/data-minifed.json")
+        fetch("https://gist.githubusercontent.com/rconnolly/d37a491b50203d66d043c26f33dbd798/raw/37b5b68c527ddbe824eaed12073d266d5455432a/clothing-compact.json")
             .then(response => {
                 if (response.ok) {
                     return response.json()
