de8ec2e87c37d71a43ed4d5833712eaa93c6ece0
Anthony Muma <amuma917@mtroyal.ca>
2025-11-25 17:04:17 -0700

    small loading circle fix?

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ import { showToast } from "./toast.js";
 import { initHome } from "./home.js";
 
 const url = "https://gist.githubusercontent.com/rconnolly/d37a491b50203d66d043c26f33dbd798/raw/37b5b68c527ddbe824eaed12073d266d5455432a/clothing-compact.json"
+
 function loadBrowse(products) {
     // Init Browse
     initBrowse(products);
